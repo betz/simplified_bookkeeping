1904529a3123de4b58db944c942b7ebae90b1d81
vagrant <vagrant@local.dashboard.hsbxl.be>
2017-08-19 21:17:45 +0000

    date process

---
+++ src/Plugin/migrate/fix_date_argenta.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Drupal\simplified_bookkeeping\Plugin\migrate\process;
+
+use Drupal\migrate\MigrateException;
+use Drupal\migrate\MigrateExecutableInterface;
+use Drupal\migrate\ProcessPluginBase;
+use Drupal\migrate\Row;
+
+/**
+ * Convert a date field to the proper format.
+ *
+ * @MigrateProcessPlugin(
+ *   id = "fix_date",
+ * )
+ */
+class FixDateArgenta extends ProcessPluginBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function transform($value, MigrateExecutableInterface $migrate_executable, Row $row, $destination_property) {
+
+    try {
+      if (is_numeric($value)) {
+        $date = new \DateTime();
+        $date->setTimestamp($value);
+      }
+      elseif (is_array($value)) {
+        $value = implode(',', $value);
+        $date = new \DateTime($value);
+      }
+      else {
+        $date = new \DateTime($value);
+      }
+      $value = $date->format('Y-m-d\TH:i:s');
+    }
+    catch (\Exception $e) {
+      throw new MigrateException('Invalid source date.');
+    }
+    return $value;
+  }
+}
