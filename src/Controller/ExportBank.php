eb14da251e424e89109bb9bc79d7c70eb35c38dd
betz <tom@behets.me>
2017-08-26 02:43:29 +0200

    Query and load all bankstatements

--- src/Controller/ExportBank.php
+++ src/Controller/ExportBank.php
@@ -3,6 +3,8 @@
 namespace Drupal\simplified_bookkeeping\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
+use Drupal\simplified_bookkeeping\Controller\BookingEntityController;
+use Drupal\simplified_bookkeeping\Entity\BookingEntity;
 
 
 /**
@@ -15,9 +17,21 @@ class ExportBank extends ControllerBase {
    */
   public function content() {
 
+    $query = \Drupal::entityQuery('booking');
+    $query->condition('status', 1);
+    $query->condition('type', 'bankstatement');
+
+    $entity_ids = $query->execute();
+    $bookings = entity_load_multiple('booking', $entity_ids);
+
+    foreach ($bookings as $booking) {
+      $view = entity_view($booking, 'full');
+      $output[] = \Drupal::service('renderer')->renderRoot($view);
+    }
+
     $build = array(
       '#type' => 'markup',
-      '#markup' => t('Hello World!'),
+      '#markup' => implode('', $output),
     );
     return $build;
   }
