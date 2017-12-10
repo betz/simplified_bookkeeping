7600f09480e2e8b5c0ffb158273b03d8e065a369
betz <tom@behets.me>
2017-12-10 19:50:35 +0100

    add default aamount value based on statement value

--- js/simplified_bookkeeping.js
+++ js/simplified_bookkeeping.js
@@ -7,6 +7,7 @@
 
       var statement_amount = $("#edit-field-booking-amount-wrapper input").val();
       var left_amount = Number(statement_amount);
+      var statement_date = $(".form-item-field-booking-date-0-value-date input").val();
 
       $(".ief-entity-table tr td.inline-entity-form-booking-amount").each(function(e, amount) {
         var line_amount = Number($(this).html());
@@ -14,8 +15,7 @@
       });
 
       $(".ief-form-bottom .form-item-field-booking-form-inline-entity-form-field-booking-amount-0-value input").val(left_amount);
-
-
+      $(".ief-form-bottom .form-item-field-booking-form-inline-entity-form-field-booking-date-0-value-date input").val(statement_date);
     }
   };
 
